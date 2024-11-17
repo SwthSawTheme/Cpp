1c39648fd6993e1204890c6963c64a56d071152d
SwthSawTheme <sawtheme1@gmail.com>
2024-11-17 15:18:28 -0300

    variables in c++

---
+++ introduction/variable.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+    //inteiros
+    int age = 25;
+    int year = 2024;
+    int days = 7.5;
+
+    //double (numeros decimais)
+    double price = 19.99;
+    double temperatue = 31.2;
+    double tileset = 2.2;
+
+    std::cout << days << std::endl;
+    std::cout << year << std::endl;
+
+    std::cout << price << std::endl;
+    std::cout << temperatue << std::endl;
+    std::cout << tileset << std::endl;
+    return 0;
+}
