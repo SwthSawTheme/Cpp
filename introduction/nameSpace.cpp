d278017f04fb9dbb9d7d4f90e78182439aec97ac
SwthSawTheme <sawtheme1@gmail.com>
2024-11-17 16:45:19 -0300

    Using Namespace, function, std

--- introduction/nameSpace.cpp
+++ introduction/nameSpace.cpp
@@ -8,9 +8,20 @@ namespace second{
     int x = 3;
 }
 
+void funcName(){
+    using namespace std;
+    cout << "Test Using Namespace in function" << endl;
+}
+
 int main(){
+    using namespace second;
+
     int x = 0;
 
+    std::cout << x << std::endl;
     std::cout << first::x << std::endl;
+    std::cout << second::x << std::endl;
+    funcName();
+
     return 0;
 }
