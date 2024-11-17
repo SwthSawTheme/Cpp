1cb86a8b298d674aeb5e11f5cd6d583e606df9c0
SwthSawTheme <sawtheme1@gmail.com>
2024-11-17 15:09:17 -0300

    Comment

--- introduction/helloworld.cpp
+++ introduction/helloworld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){
+    // Após o texto é possível usar << '\n' para finalizar a linha
     std::cout << "I like pizza!" << std::endl;
     std::cout << "It's really good!" << std::endl;
     return 0;
