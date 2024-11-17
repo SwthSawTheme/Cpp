06609281354a0b43caf51c20c29492e83a9aabb8
SwthSawTheme <sawtheme1@gmail.com>
2024-11-17 15:23:29 -0300

    Caracteres unicos e valores boleanos

--- introduction/variable.cpp
+++ introduction/variable.cpp
@@ -12,6 +12,18 @@ int main(){
     double temperatue = 31.2;
     double tileset = 2.2;
 
+    // Unico caracter
+    char grade = 'A';
+    char initial = 'b';
+    char singReal = 'R';
+
+    // Valor Boleano
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    
+
     std::cout << days << std::endl;
     std::cout << year << std::endl;
 
