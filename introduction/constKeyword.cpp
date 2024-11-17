4cc7308a0cf0a1b013b8ee60a85f06d01610f984
SwthSawTheme <sawtheme1@gmail.com>
2024-11-17 16:19:49 -0300

    Programa simplificado de calculo de radianos

---
+++ introduction/constKeyword.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    double pi = 3.14159;
+    double radius = 10;
+    double circumference = 2* pi * radius;
+
+    std::cout << circumference << std::endl;
+    return 0;
+}
