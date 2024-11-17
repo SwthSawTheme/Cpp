1b3e7112422d8fb14cd67892b0863237fd121c40
SwthSawTheme <sawtheme1@gmail.com>
2024-11-17 16:24:00 -0300

    Criação de variaves constantes, e uso de tipo long long

--- introduction/constKeyword.cpp
+++ introduction/constKeyword.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
 int main(){
-    double pi = 3.14159;
+    // Tornando a variavel constante
+    const double pi = 3.14159;
     double radius = 10;
     double circumference = 2* pi * radius;
+
+    const long long LIGHT_SPEED = 283773777723;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
 
     std::cout << circumference << std::endl;
     return 0;
