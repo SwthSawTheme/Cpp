2e4ec2dd0ba5e08e2e447593a339eaec3695f6e7
SwthSawTheme <sawtheme1@gmail.com>
2024-11-19 20:40:51 -0300

    cmath in cpp

--- introduction/mathFunction.cpp
+++ introduction/mathFunction.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 int main(){
     double x = 8.5;
@@ -6,7 +7,13 @@ int main(){
     double z;
 
     // z = std::max(x,y);
-    z = std::min(x,y);
+    // z = std::min(x,y);
+    // z = pow(2,3);
+    // z = sqrt(9);
+    // z = abs(-3);
+    // z = round(x);
+    // z = floor(x);
+    z = ceil(x);
 
     std::cout << z << std::endl;
     return 0;
