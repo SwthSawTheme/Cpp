ccf58e83ce5de45c1e7eca8ba9bd6dc2ea65cfc1
SwthSawTheme <sawtheme1@gmail.com>
2024-11-19 23:52:12 -0300

    Adicionado comentarios

--- practicing/calc.cpp
+++ practicing/calc.cpp
@@ -1,72 +1,85 @@
 #include <iostream>
 
-
+// Função para somar dois números
 int opSum(double num1, double num2) {
     return num1 + num2;
 }
 
+// Função para subtrair dois números
 int opSub(double num1, double num2) {
     return num1 - num2;
 }
 
+// Função para multiplicar dois números
 int opMult(double num1, double num2) {
     return num1 * num2;
 }
 
+// Função para dividir dois números
 int opDiv(double num1, double num2) {
     return num1 / num2;
 }
 
-int main(){
+int main() {
+    // Variáveis para armazenar o operador e os dois números de entrada
     char op;
     double num1;
     double num2;
 
+    // Exibe o título do programa
     std::cout << "======== Calculadora ========" << std::endl;
 
+    // Solicita ao usuário o operador desejado
     std::cout << "Digite um operador ( * / + -): ";
     std::cin >> op;
 
-    switch (op){
+    // Estrutura de decisão para executar a operação escolhida
+    switch (op) {
 
-        case '*':
+        case '*': // Caso o operador seja multiplicação
             std::cout << "Digite um numero: ";
             std::cin >> num1;
             std::cout << "Digite outro numero: ";
             std::cin >> num2;
 
-            std::cout << "O resultado de " << num1 << "*" << num2 << " = " << opMult(num1,num2) << std::endl;
+            // Exibe o resultado da multiplicação
+            std::cout << "O resultado de " << num1 << "*" << num2 << " = " << opMult(num1, num2) << std::endl;
             break;
 
-        case '/':
+        case '/': // Caso o operador seja divisão
             std::cout << "Digite um numero: ";
             std::cin >> num1;
             std::cout << "Digite outro numero: ";
             std::cin >> num2;
 
-            std::cout << "O resultado de " << num1 << "/" << num2 << " = " << opDiv(num1,num2) << std::endl;
+            // Exibe o resultado da divisão
+            std::cout << "O resultado de " << num1 << "/" << num2 << " = " << opDiv(num1, num2) << std::endl;
             break;
 
-        case '+':
+        case '+': // Caso o operador seja soma
             std::cout << "Digite um numero: ";
             std::cin >> num1;
             std::cout << "Digite outro numero: ";
             std::cin >> num2;
 
-            std::cout << "O resultado de " << num1 << "+" << num2 << " = " << opSum(num1,num2) << std::endl;
+            // Exibe o resultado da soma
+            std::cout << "O resultado de " << num1 << "+" << num2 << " = " << opSum(num1, num2) << std::endl;
             break;
 
-        case '-':
+        case '-': // Caso o operador seja subtração
             std::cout << "Digite um numero: ";
             std::cin >> num1;
             std::cout << "Digite outro numero: ";
             std::cin >> num2;
 
-            std::cout << "O resultado de " << num1 << "-" << num2 << " = " << opSub(num1,num2) << std::endl;
+            // Exibe o resultado da subtração
+            std::cout << "O resultado de " << num1 << "-" << num2 << " = " << opSub(num1, num2) << std::endl;
             break;
-        default:
+
+        default: // Caso o operador digitado seja inválido
             std::cout << "Digite um operador valido ( * / + -)!" << std::endl;
     }
-    
+
+    // Exibe o final do programa
     std::cout << "=============================";
 }
