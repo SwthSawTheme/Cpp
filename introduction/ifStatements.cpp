5e5704e89f39a3a61a97fcf2cc0fc29860fc9711
SwthSawTheme <sawtheme1@gmail.com>
2024-11-19 22:49:28 -0300

    testes com if composto,uso de else if

--- introduction/ifStatements.cpp
+++ introduction/ifStatements.cpp
@@ -6,9 +6,16 @@ int main() {
     std::cout << "Digite sua idade: ";
     std::cin >> age;
 
-    if (age >= 18) {
+    if (age >= 100) {
+        std::cout << "Voce esta a cima da populacao mundial!";
+    }
+    else if (age >= 18){
         std::cout << "Voce e maior de idade";
-    } else {
+    }
+    else if (age < 8) {
+        std::cout << "Voce e muito novo!";
+    }
+    else {
         std::cout << "Voce e menor de idade";
     }
 
