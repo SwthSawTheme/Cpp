dd72b9cb638ce8bf2a989a87a608985b2673ed24
SwthSawTheme <sawtheme1@gmail.com>
2024-11-20 00:00:50 -0300

    comment

--- practicing/ParImpar.cpp
+++ practicing/ParImpar.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 int main() {
-    int num = 7;
-    std::cout << (num % 2 == 0 ? "Par" : "Ímpar") << std::endl;
+    int num = 4;
+    // Teste simplificado usando operador ternario "?"
+    std::cout << (num % 2 == 0 ? "Par" : "Impar") << std::endl;
 
     return 0;
 }
